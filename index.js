e68aa4bbf96ba248f5d527ec37670cf025d49811
Amansinghbatola <amanbatola2004@gmail.com>
2025-08-06 15:35:38 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const dropZone = document.querySelector(".drop-zone");
 const fileInput = document.querySelector("#fileInput");
 const browseBtn = document.querySelector("#browseBtn");
+
+const bgProgress = document.querySelector(".bg-progress");
+const progressPercent = document.querySelector("#progressPercent");
+const progressContainer = document.querySelector(".progress-container");
+const progressBar = document.querySelector(".progress-bar");
+const status = document.querySelector(".status");
