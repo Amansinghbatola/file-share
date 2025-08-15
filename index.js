8fe8b0932631457d001c17d80409ed0a09d4e3ae
Amansinghbatola <amanbatola2004@gmail.com>
2025-08-15 19:01:11 +0530

    update

--- index.js
+++ index.js
@@ -164,12 +164,4 @@ emailForm.addEventListener("submit", (e) => {
 });
 
 let toastTimer;
-// the toast function
-const showToast = (msg) => {
-  clearTimeout(toastTimer);
-  toast.innerText = msg;
-  toast.classList.add("show");
-  toastTimer = setTimeout(() => {
-    toast.classList.remove("show");
-  }, 2000);
-};
+// the
